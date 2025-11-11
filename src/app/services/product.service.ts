2f351c70e9a99eb01057d016f1568db5261a9492
Claire <claire.cy.t@gmail.com>
2025-11-11 15:37:49 -0500

    add dynamic product category menu

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,10 +3,16 @@ import { Injectable } from '@angular/core';
 import { response } from 'express';
 import { map, Observable } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  };
+}
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategories: ProductCategory[];
   };
 }
 
@@ -14,14 +20,21 @@ interface GetResponse {
   providedIn: 'root'
 })
 export class ProductService {
-  private baseUrl = 'http://localhost:8080/api/products';
+
+  private baseUrl = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-    return this.http.get<GetResponse>(searchUrl).pipe(
+    const searchUrl = `${this.baseUrl}/products/search/findByCategoryId?id=${theCategoryId}`;
+    return this.http.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.http.get<GetResponseProductCategory>(`${this.baseUrl}/product-category`).pipe(
+      map(response => response._embedded.productCategories)
     );
   }
 }
